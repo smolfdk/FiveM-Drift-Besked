ab09f6a1037ef8a489042d0560a0b8148bd24468
Fr3ddieDK <92995160+Fr3ddieDK@users.noreply.github.com>
2021-12-16 19:33:32 +0100

    Update server-log.lua

--- server-log.lua
+++ server-log.lua
@@ -10,7 +10,7 @@ function sendToDiscord(farve, titel, besked, undertext)
           }
       }
   
-    PerformHttpRequest('https://discord.com/api/webhooks/921089748146200666/5WXvln9eT6oBos8K0JybZg3lFfnaapn7cktDqJiSkvCIcmMwEQm4EDobOlHlgDZmx_Zk', function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest('DIN-WEBHOOK', function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
   end
 
 sendToDiscord(16711680, "Server Drift", "```connect din-server-ip```", "Lavet af Fr3ddie#0001")
